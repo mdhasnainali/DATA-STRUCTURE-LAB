6223a08bf0f1c1fb956ecd094afce61c982a4a45
mdhasnainali <mdhasnainali.01@gmail.com>
2022-05-20 09:14:42 +0600

    initialize

---
+++ Array, Records and Pointers/Python/traversing.py
@@ -0,0 +1,13 @@
+
+arr = [3,24,46,2,56,24,12,646,12,35,464,121]
+
+def get_maximum(arr):
+    max = arr[0]
+    for i in arr:
+        if(i>max):
+            max = i;
+    return max
+
+
+def get_minimum(arr):
+    
